e332d7691626c1029020bf8134a51175882d805d
jacky09299 <66111556+jacky09299@users.noreply.github.com>
2024-04-20 18:36:41 +0800

    Create pose.py

---
+++ pose.py
@@ -0,0 +1,11 @@
+from mmpose.apis import MMPoseInferencer
+
+
+# 使用模型别名创建推理器
+inferencer = MMPoseInferencer(pose3d="human3d")
+
+
+folder_path = 0
+
+result_generator = inferencer(folder_path, show=True, use_oks_tracking=True)
+results = [result for result in result_generator]
